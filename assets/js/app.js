6484778e94ef12994015ef7982e7bb71dd20ae14
HockingM <64179310+HockingM@users.noreply.github.com>
2020-09-11 17:11:17 +1000

    Update app.js
    import csv

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,13 @@
-// @TODO: YOUR CODE HERE!
+var censusData // will default to undefined
+
+// load data from data.csv (onto local server)
+d3.csv("./assets/data/data.csv").then(function(censusData) {
+
+  console.log(censusData);
+
+  // log a list of states
+  var state = censusData.map(data => data.state); // for list of names
+  console.log("state", state);
+  
+});
+
